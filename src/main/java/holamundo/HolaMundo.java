4d430138b49c4dbfe29de1b6b682db5ab7d31741
Sebastian211-alt <ffranshesco119@gmail.com>
2022-03-10 17:12:19 -0600

    Se agrega el Proyecto

---
+++ src/main/java/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+    
+    public static void main(String[] args){
+        
+        System.out.println(" HOLA MUNDO ESTE ES MI PRIMER INTENTO EN JAVA ");
+        
+    }
+    
+}
